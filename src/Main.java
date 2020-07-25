50849bde7178c5e0f8bf918d89b41ebd71c1be1c
Alexey <ter-am@mail.ru>
2020-07-25 22:16:35 +0600

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,28 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
-public class Main {
-    public static void main(String[] args) {
-        //D:/TERAMEDIA/...
-        String folderPath = "D:/TERAMEDIA/CLIPARTS";
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ParametersBag bag = new ParametersBag(args);
+
+        String folderPath = bag.getPath();
+        long sizeLimit = bag.getLimit();
+
         File file = new File(folderPath);
+        Node root = new Node(file, sizeLimit);
 
         long start = System.currentTimeMillis();
-        System.out.println(getFoldersSize(file));
-        long duration = (System.currentTimeMillis() - start)/1000;
-        System.out.println(duration + "seconds");
-    }
 
-    public static long getFoldersSize(File folder) {
-        if (folder.isFile()) {
-            return folder.length();
-        }
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for (File file : files) {
-            sum += getFoldersSize(file);
-        }
-        return sum;
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(root);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(calculator);
+
+        System.out.println(root);
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
     }
 }
